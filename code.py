471aa8834fc32c18fafbcf6bedb3cdec3b57856d
BethelDaniel <109989682+BethelDaniel@users.noreply.github.com>
2023-05-15 09:34:06 -0500

    Update code.py

--- code.py
+++ code.py
@@ -2,12 +2,11 @@ import random
 
 while True:
     guess = random.randint(1,5)
-    player = input("guess a number: ")
-    if TypeError or ValueError:
-        print("please try again")
-        break
-    if guess != int(player):
-        print(guess)
-        continue
-    else:
+    player =input("guess a number: ")
+    try:
+        if guess != int(player):
+            print(guess)
+            continue
+    except:
+        print("wrong type")
         break
